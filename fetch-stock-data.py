955257e19319d9e2ae6b62daf1ec8caa4c86b682
kjkunte <kjkunte@gmail.com>
2024-08-01 23:47:08 +0530

    crossover

---
+++ fetch-stock-data.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+
+# List of stock codes
+stock_codes = [
+    'RELIANCE.NS', 'TATAMOTORS.NS', 'ADANIENT.NS', 'BAJFINANCE.NS', 'SBIN.NS',
+    'HDFCBANK.NS', 'ICICIBANK.NS', 'KOTAKBANK.NS', 'AXISBANK.NS', 'INDUSINDBK.NS',
+    'PIDILITIND.NS', 'VOLTAS.NS', 'L&TFH.NS', 'TATACHEM.NS', 'GODREJPROP.NS',
+    'TCS.NS', 'INFY.NS', 'LT.NS', 'SUNPHARMA.NS', 'M&M.NS'
+]
+
+# Download stock data
+stock_data = yf.download(stock_codes, start='2020-01-01', end='2023-01-01')
+
+# Display data
+print(stock_data.head())
